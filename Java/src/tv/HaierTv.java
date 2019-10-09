a14be2fae3d548a726ff1aa15f1fad8a1092d830
lyylyylyyl <54982580+lyylyylyyl@users.noreply.github.com>
2019-10-09 22:04:25 +0800

    java

---
+++ Java/src/tv/HaierTv.java
@@ -0,0 +1,8 @@
+package tv;
+
+public class HaierTv extends TV{
+	public void play() {
+		System.out.println("HaierTv");
+	}
+
+}
---
+++ Java/src/tv/HisenseTv.java
@@ -0,0 +1,8 @@
+package tv;
+
+public class HisenseTv extends TV {
+	public void play() {
+		System.out.println("HisenseTv");
+	}
+
+}
