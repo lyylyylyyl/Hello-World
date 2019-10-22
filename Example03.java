c642f925b96f27801f7678d3a2b6705c1e19e0f8
lyylyylyyl <54982580+lyylyylyyl@users.noreply.github.com>
2019-10-22 16:35:20 +0800

    5-3 Example03 实现Runnable接口

--- Example03.java
+++ Example03.java
@@ -1,20 +1,19 @@
-
- class Single {
-	private static Single INSTANCE =new Single();
-	private Single() {}
-	public static Single getInstance() {
-		return INSTANCE;
-	}
-
-}
+package xian;
 
 public class Example03 {
 
 	public static void main(String[] args) {
-		Single s1=Single.getInstance();
-		Single s2=Single.getInstance();
-		System.out.println(s1==s2);
-
+		// TODO 自动生成的方法存根
+		MyThread mythread=new MyThread();
+		Thread thread=new Thread(mythread);
+		thread.start();
+		while(true) {
+			System.out.println("main方法在执行");
+		}
 	}
-
+}
+class MyThread implements Runnable{
+	public void run() {
+		System.out.println("RUN方法");
+	}
 }
