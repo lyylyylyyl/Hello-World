1efc1e484cc27c7cd21b61fcd49a93b359508416
lyylyylyyl <54982580+lyylyylyyl@users.noreply.github.com>
2019-10-22 16:58:35 +0800

    编程题1

---
+++ One.java
@@ -0,0 +1,19 @@
+package xian;
+
+public class One {
+
+	public static void main(String[] args) {
+		new MyThread("Thread1").start();
+		new MyThread("Thread2").start();
+
+	}
+
+}
+class MyThread extends Thread{
+	public MyThread(String name) {
+		super(name);
+	}
+	public void run() {
+		System.out.println(this.getName());
+	}
+}
