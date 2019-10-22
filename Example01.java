8324735fa8dc4d3601bf2b7566d9a21f6d421d61
lyylyylyyl <54982580+lyylyylyyl@users.noreply.github.com>
2019-10-22 16:28:31 +0800

    5-1 Example01  单线程

--- Example01.java
+++ Example01.java
@@ -1,35 +1,21 @@
-class Student {
-	private String name;
-	private int grade;
-	public String getName() {
-		return name;
-	}
-	public void setName(String stuName) {
-		name=stuName;
-	}
-	public int getGrade() {
-		return grade;
-	}
-	public void setGrade(int stuGrade) {
-		if(stuGrade<0) {
-			System.out.println("输入成绩为负！！！");
-		}else {
-			grade=stuGrade;
-		}
-	}
-	public void introduce() {
-		System.out.println("我叫"+name+"，成绩为"+grade);
-	}
+package xian;
 
-}
 public class Example01 {
 
 	public static void main(String[] args) {
-		Student stu=new Student();
-		stu.setName("李洋");
-		stu.setGrade(99);
-		stu.introduce();
+		MyThread mythread=new MyThread();
+		mythread.run();
+		while(true) {
+			System.out.println("Main方法在运行");
+		}
 
 	}
 
+}
+class MyThread{
+	public void run() {
+		while(true) {
+			System.out.println("Run方法在运行");
+		}
+	}
 }
