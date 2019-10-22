74316205479e245b2f252375c923469058af4c83
lyylyylyyl <54982580+lyylyylyyl@users.noreply.github.com>
2019-10-22 17:38:10 +0800

    5位司机接客人

---
+++ TicketWindow.java
@@ -0,0 +1,15 @@
+package xian;
+
+class TicketWindow implements Runnable{
+	private int ticket=100;
+	public void run() {
+		while(true) {
+			if(ticket>0) {
+				Thread th=Thread.currentThread();
+				String th_name=th.getName();
+				System.out.println(th_name+"正在发售第"+ticket--+"张票");
+			}
+		}
+	}
+
+}
