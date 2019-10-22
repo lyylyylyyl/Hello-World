766c2246157ca135324c8e62da72a34b3ba763d5
lyylyylyyl <54982580+lyylyylyyl@users.noreply.github.com>
2019-10-22 16:51:47 +0800

    5-5 共同售100张票

---
+++ Example05.java
@@ -0,0 +1,26 @@
+package xian;
+
+public class Example05 {
+
+	public static void main(String[] args) {
+		TicketWindow tw=new TicketWindow();
+		new Thread(tw,"窗口1").start();
+		new Thread(tw,"窗口2").start();
+		new Thread(tw,"窗口3").start();
+		new Thread(tw,"窗口4").start();
+	}
+
+}
+class TicketWindow implements Runnable{
+	private int ticket=100;
+	public void run() {
+		while(true) {
+			if(ticket>0) {
+				Thread th=Thread.currentThread();
+				String th_name=th.getName();
+				System.out.println(th_name+"正在发售第"+ticket--+"张票");
+			}
+		}
+	}
+
+}
