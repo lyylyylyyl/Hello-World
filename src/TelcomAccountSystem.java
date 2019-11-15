6dc43524e859864ac45a53200160bf190955e100
lyylyylyyl <54982580+lyylyylyyl@users.noreply.github.com>
2019-11-15 09:00:05 +0800

    十一周作业模拟计费

---
+++ src/TelcomAccountSystem.java
@@ -0,0 +1,13 @@
+
+public class TelcomAccountSystem {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		TelcomUser telcomUser=new TelcomUser("15837206666");
+		telcomUser.generateCommunicateRecord();
+		telcomUser.printDetails();
+				
+
+	}
+
+}
